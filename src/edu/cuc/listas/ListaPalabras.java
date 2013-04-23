ac5c51d867c6acd96d57ba799dd479079f82138e
Jedabero <jedabero@hotmail.com>
2013-04-23 12:14:39 -0500

    Added listass

---
+++ src/edu/cuc/listas/ListaPalabras.java
@@ -0,0 +1,157 @@
+
+package edu.cuc.listas;
+
+
+/**
+ *
+ * @author jberdugo10
+ */
+public class ListaPalabras {
+    
+    private String[] listadoPalabras;
+    private int indicePalabra;
+    
+    public ListaPalabras(int dimLista) throws Exception {
+        if (dimLista>0) {
+            listadoPalabras = new String[dimLista];
+            indicePalabra = 0;
+        } else {
+            throw new Exception("Dimensión Invalida");
+        }
+    }
+    
+    public boolean adicionarPalabra(String palabraNueva){
+        if (!palabraNueva.isEmpty()) {
+            if (indicePalabra<listadoPalabras.length) {
+                listadoPalabras[indicePalabra] = palabraNueva;
+                indicePalabra++;
+                return true;
+            } else {
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    public int buscarPos(String palabraBusqueda){
+        if (!palabraBusqueda.isEmpty()) {
+            for (int i = 0; i < indicePalabra; i++) {
+                if (palabraBusqueda.equals(listadoPalabras[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        } else {
+            return -1;
+        }
+    }
+    
+    public boolean buscar(String palabraBusqueda){
+        if (!palabraBusqueda.isEmpty()) {
+            for (int i = 0; i < indicePalabra; i++) {
+                if (palabraBusqueda.equals(listadoPalabras[i])) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean eliminar(String palabraEliminar){
+        if (!palabraEliminar.isEmpty()) {
+            int pos = buscarPos(palabraEliminar);
+            if (pos != -1) {
+                for (int i = pos; i < indicePalabra; i++) {
+                    listadoPalabras[i] = listadoPalabras[i+1];
+                }
+                indicePalabra--;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return visualizarLista();
+    }
+    
+    public String visualizarLista(){
+        String lista = "\n";
+        for (int i = 0; i < indicePalabra; i++) {
+            lista += (i+1)+". "+listadoPalabras[i]+"\n";
+        }
+        return lista;
+    }
+    
+    public int contarPalabras(String palabra, int cantidad){
+        if (!palabra.isEmpty()) {
+            if (indicePalabra>0) {
+                int cont = 0;
+                for (int i = 0; i < indicePalabra; i++) {
+                    if (palabra.length() == listadoPalabras[i].length()) {
+                        cont++;
+                    }
+                }
+                return cont;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+        
+    }
+    
+    public double promedioPalabras() throws Exception {
+        if (indicePalabra>0) {
+            double sum = 0;
+            for (int i = 0; i < indicePalabra; i++) {
+                sum += listadoPalabras[i].length();
+            }
+            return sum/indicePalabra;
+        } else {
+            throw new Exception("Lista Vacía");
+        }
+    }
+    
+    public String contarElementosPromedio(String palabra) throws Exception{
+        String lista = "";
+        if(!palabra.isEmpty()){
+            double promedioActual = promedioPalabras();
+            for (int i = 0; i < indicePalabra; i++) {
+                if (listadoPalabras[i].length()>promedioActual) {
+                    lista += "\n"+listadoPalabras[i];
+                }
+            }
+            lista += "\n";
+            return lista;
+        }else{
+            throw new Exception("Cadena vacía");
+        }
+        
+    }
+    
+    public ListaPalabras palabrasComunes(ListaPalabras lista1,
+            ListaPalabras lista2)throws Exception{
+        ListaPalabras listaComun = new ListaPalabras(20);
+        
+        if ((lista1!=null)&&(lista2!=null)) {
+            for (int i = 0; i < indicePalabra; i++) {
+                if (lista2.buscar(lista1.listadoPalabras[i])) {
+                    listaComun.adicionarPalabra(lista1.listadoPalabras[i]);
+                }
+            }
+            return listaComun;
+        } else {
+            throw new Exception("Lista nula");
+        }
+    }
+    
+}
---
+++ src/pruebas/PruebaLista.java
@@ -0,0 +1,67 @@
+
+package pruebas;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import edu.cuc.listas.ListaPalabras;
+
+/**
+ *
+ * @author jberdugo10
+ */
+public class PruebaLista {
+    
+    public static void main(String args[]){
+        ListaPalabras miLista = null;
+        ListaPalabras miLista2 = null;
+        try {
+            miLista = new ListaPalabras(10);
+            miLista2 = new ListaPalabras(10);
+        } catch (Exception ex) {
+            Logger.getLogger(PruebaLista.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        miLista.adicionarPalabra("Hola");
+        miLista.adicionarPalabra("Clase");
+        miLista.adicionarPalabra("De");
+        miLista.adicionarPalabra("Programación");
+        
+        System.out.println("Buscar Hola: "+miLista.buscar("Hola"));
+        System.out.println("Buscar Mundo: "+miLista.buscar("mundo"));
+        System.out.println("Lista: "+miLista);
+        
+        miLista.eliminar("De");
+        System.out.println("Lista: "+miLista);
+        
+        miLista.eliminar("Mundo");
+        System.out.println("Lista: "+miLista);
+        
+        System.out.println("Contar la palabra Clase: "
+                + miLista.contarPalabras("Clase", 3));
+        
+        try {
+            System.out.println("Promedio palabras: "
+                    + miLista.promedioPalabras());
+            System.out.println("Elementos con longitud por encima del promedio: "
+                    + miLista.contarElementosPromedio("Clase"));
+        } catch (Exception ex) {
+            Logger.getLogger(PruebaLista.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        miLista2.adicionarPalabra("Hola");
+        miLista2.adicionarPalabra("Clase");
+        miLista2.adicionarPalabra("De");
+        miLista2.adicionarPalabra("Calculo");
+        miLista2.adicionarPalabra("Vectorial");
+        try {
+            System.out.println("Palabras comunes entre\n"+miLista+"\ny\n"+miLista2
+                    + "\nes:\n"+miLista.palabrasComunes(miLista, miLista2));
+        } catch (Exception ex) {
+            Logger.getLogger(PruebaLista.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
