ef406b7ccda7fbab6360133458cd6d6c07806aad
Jedabero <jedabero@hotmail.com>
2013-04-23 16:12:16 -0500

    Added methods to Mago

--- src/edu/cuc/juegorol/Mago.java
+++ src/edu/cuc/juegorol/Mago.java
@@ -7,7 +7,7 @@ package edu.cuc.juegorol;
  */
 public class Mago extends Personaje {
     
-    
+    private String hechizos[] = new String[4];
     
     public Mago(String nombre, int fuerza, int intel, int hp){
         super(nombre, "mago", fuerza, intel, hp);
@@ -17,5 +17,39 @@ public class Mago extends Personaje {
             setFuerza(15);
         }
     }
+    
+    public int nuevoHechizo(String hechizo){
+        for (int i = 0; i < hechizos.length; i++) {
+            if(hechizos[i] != null){
+                hechizos[i] = hechizo;
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public boolean eliminarHechizo(int n) throws Exception{
+        if(n<4){
+            hechizos[n] = null;
+            return true;
+        }else{
+            throw new Exception("No hay tal hechizo");
+        }
+        
+    }
+    
+    public boolean atacar(Personaje p, int numHecz) throws Exception{
+        int hp = p.getPuntosVida();
+        
+        if(eliminarHechizo(numHecz)){
+            p.setPuntosVida(hp-10);
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+    
+    
     
 }
