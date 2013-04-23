bd3d1fbf41c14040dcba78d91154a07666cf951a
Jedabero <jedabero@hotmail.com>
2013-04-23 15:36:00 -0500

    Added Mago and Clerigo

---
+++ src/edu/cuc/juegorol/Clerigo.java
@@ -0,0 +1,15 @@
+
+package edu.cuc.juegorol;
+
+/**
+ *
+ * @author Jedabero
+ */
+public class Clerigo extends Personaje {
+    
+    public Clerigo(String nombre, int fuerza, int intel, int hp){
+        super(nombre, "mago", fuerza, intel, hp);
+        
+    }
+    
+}
---
+++ src/edu/cuc/juegorol/Mago.java
@@ -0,0 +1,17 @@
+
+package edu.cuc.juegorol;
+
+/**
+ *
+ * @author Jedabero
+ */
+public class Mago extends Personaje {
+    
+    
+    
+    public Mago(String nombre, int fuerza, int intel, int hp){
+        super(nombre, "mago", fuerza, intel, hp);
+        
+    }
+    
+}
