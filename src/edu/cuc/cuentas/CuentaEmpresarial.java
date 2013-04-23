9295f2e89a54c413aceb9288292929b17bffb66b
Jedabero <jedabero@hotmail.com>
2013-04-23 12:32:57 -0500

    Added CuentaEmpresarial

---
+++ src/edu/cuc/cuentas/CuentaEmpresarial.java
@@ -0,0 +1,20 @@
+
+package edu.cuc.cuentas;
+
+/**
+ *
+ * @author Jedabero
+ */
+public class CuentaEmpresarial extends Cuenta{
+    
+    private String numeroChequera;
+    private double cupoSobreGiro;
+    
+    public CuentaEmpresarial(Titular titular, String numCuenta, String numCheq,
+            double cupSobGir){
+        super(titular, numCuenta);
+        this.numeroChequera = numCheq;
+        this.cupoSobreGiro = cupSobGir;
+    }
+    
+}
