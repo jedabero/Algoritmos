0791ed48622807a3427f52e9a1a6feaaf7c3c818
jedabero <jedabero@hotmail.com>
2013-06-29 12:53:25 -0500

    Started Pilas y Colas. Added class Dato.

---
+++ src/edu/cuc/listas/Dato.java
@@ -0,0 +1,16 @@
+package edu.cuc.listas;
+
+/**
+ *
+ * @author jedabero
+ */
+public class Dato {
+    
+    private int valor;
+    private int id;
+    
+    public Dato(int valor, int id){
+        this.valor = valor;
+        this.id = id;
+    }
+}
